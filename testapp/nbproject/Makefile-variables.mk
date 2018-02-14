05ef978e9304aa7e8b28a57b5258125ac1a4fbef
sneevilman <sneevilman@gmail.com>
2018-02-14 13:38:27 -0800

    setting up and reorganizing

---
+++ testapp/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=testapp
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/testapp
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=testapp.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/testapp.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=testapp
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/testapp
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=testapp.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/testapp.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
